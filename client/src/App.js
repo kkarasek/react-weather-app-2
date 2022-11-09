6a00ba75f1f4f73dbda4d1edfddb59cbd8765fbb
Jakub Karasek <jkarasek@Jakubs-MacBook-Air.local>
2022-11-09 20:04:01 +0100

    Initial deploy

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,40 @@
-function App() {
-	return <div className="App">Hello World!</div>;
-}
+import styled from 'styled-components';
+
+const Container = styled.div`
+	display: flex;
+	flex-direction: column;
+	margin: auto;
+	align-items: center;
+	box-shadow: 0 3px 6px 0 #555;
+	padding: 20px 10px;
+	border-radius: 4px;
+	width: 380px;
+	background: #f9f9f9;
+`;
+
+const AppTitle = styled.span`
+	font-weight: bold;
+	font-size: 18px;
+`;
+
+const CitySearch = styled.div`
+	display: flex;
+	flex-direction: column;
+`;
+
+const WeatherDisplay = styled.div`
+	display: flex;
+	flex-direction: column;
+`;
+
+const App = () => {
+	return (
+		<Container>
+			<AppTitle>Weather App</AppTitle>
+			<CitySearch>City Search</CitySearch>
+			<WeatherDisplay>Weather Info</WeatherDisplay>
+		</Container>
+	);
+};
 
 export default App;
